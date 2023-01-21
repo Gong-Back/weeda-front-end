fbcef141bab3fda6b78a54d9f1f25ac23ae3e020
ballsona0925 <ballsona0925@naver.com>
2023-01-22 03:25:09 +0900

    feat: add storybook for UI test

---
+++ storybook/storyLoader.js
@@ -0,0 +1,15 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require('../src/components/TestButton/TestButton.stories');
+}
+
+const stories = ['../src/components/TestButton/TestButton.stories'];
+
+module.exports = {
+  loadStories,
+  stories,
+};
