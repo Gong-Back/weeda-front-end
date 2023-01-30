08f60b8bb644bb86fbaf36f263dfffdd2671c418
RookieAND <gwangin1999@naver.com>
2023-01-30 21:40:29 +0900

    WEEDA-57 [fix]: change interface's name to follow camelCase
    API에 사용되는 인터페이스의 이름을 camelCase를 준수하도록 수정하였습니다.
    interface의 JSDoc 또한 함수의 매개변수를 의미하는 @param 을 사용하지 않고 속성에 주석을 두었습니다.

--- src/constants/types/APITypes.ts
+++ src/constants/types/APITypes.ts
@@ -2,18 +2,18 @@
  * API 호출 결과 (성공, 실패) 를 나타내는 type
  * @param T 요청 성공 시 인계 받은 데이터 타입
  */
-export type APIResponse<T> = Promise<
-  { isSuccess: true; result: T } | { isSuccess: false; result: APIError }
+export type ApiResponse<T> = Promise<
+  { isSuccess: true; result: T } | { isSuccess: false; result: ApiError }
 >;
 
 /**
  * API 호출 과정에서 발생한 에러와 관련된 interface
- * @param code 백엔드 측에서 전송한 HTTP Status Code
- * @param msg 백엔드 측에서 전송한 에러 관련 메세지
- * @param data 백엔드 측에서 전송한 에러 관련 데이터
  */
-export interface APIError {
+export interface ApiError {
+  /** 백엔드 측에서 전송한 HTTP Status Code */
   code: number;
+  /** 백엔드 측에서 전송한 에러 관련 메세지 */
   msg: string;
+  /** 백엔드 측에서 전송한 에러 관련 데이터 */
   data?: any;
 }
