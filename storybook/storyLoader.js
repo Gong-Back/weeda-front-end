4daf57fda575d82457707a9d4c97942c8d965ae6
ballsona0925 <ballsona0925@naver.com>
2023-02-01 16:58:21 +0900

    WEEDA-31 [feat]: add RegisterScreen

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -7,6 +7,8 @@ function loadStories() {
   require('../src/components/auth/AuthForm/AuthForm.stories');
   require('../src/components/auth/AuthHeader/AuthHeader.stories');
   require('../src/components/auth/LoginTemplate/LoginTemplate.stories');
+  require('../src/components/auth/RegisterProgressBar/RegisterProgressBar.stories');
+  require('../src/components/auth/RegisterTemplate/RegisterTemplate.stories');
   require('../src/components/common/Button/Button.stories');
   require('../src/components/common/Text/Text.stories');
   require('../src/components/common/TextInput/TextInput.stories');
@@ -16,6 +18,8 @@ const stories = [
   '../src/components/auth/AuthForm/AuthForm.stories',
   '../src/components/auth/AuthHeader/AuthHeader.stories',
   '../src/components/auth/LoginTemplate/LoginTemplate.stories',
+  '../src/components/auth/RegisterProgressBar/RegisterProgressBar.stories',
+  '../src/components/auth/RegisterTemplate/RegisterTemplate.stories',
   '../src/components/common/Button/Button.stories',
   '../src/components/common/Text/Text.stories',
   '../src/components/common/TextInput/TextInput.stories',
